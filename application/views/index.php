aa690214456b1d311f1b774b136d96d78e83d08c
bersuad <bersuadane@gmail.com>
2020-08-27 15:19:46 +0300

    interface for id

---
+++ application/views/index.php
@@ -0,0 +1,37 @@
+    <div class="row" style="width: 100%;">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <div class="input-field">
+                <input id="auto_comp" class="form-control autocomplete" type="text" placeholder="Type ID"/>
+            </div>
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+    <div class="id-card-wrapper">
+        <div class="id-card">
+            <div class="profile-row">
+            <div class="desc">
+                <div id="shape_list"></div>
+                <div id="shape_list_3"></div>
+                <div id="shape_list_4"></div>
+                <!-- <div id="shape_list_2"></div> -->
+                <div class="dp">
+                    <!-- <div class="dp-arc-outer"></div>
+                    <div class="dp-arc-inner"></div> -->
+                    <img src="<?php echo site_url() ?>/assets/image/user.jpg">
+                </div>
+                <div align="center">
+                    <br>
+                    <h3 align="center">Abebe Kebede</h3>
+                    <div style="text-align: left; padding-left: 73px;">
+                        <br>
+                        <p>Id: B-13456</p>
+                        <p>Address: Bole</p>
+                        <p>Phone N<sup><u>o</u></sup>: +251-9-123-45-678</p>
+                        <p>Issue Date: <?php echo date('d-M-Y') ?></p>
+                    </div>
+                </div>
+            </div>
+            </div>
+        </div>
+    </div>
